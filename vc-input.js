796aaef1524f4f08330d38d7e7fc90d8ac7fcc5e
wsxgun <14741171114@163.com>
2023-06-01 12:17:30 +0800

    Create vc-input.js
    ffff

---
+++ vc-input.js
@@ -0,0 +1,17 @@
+function submitVC() {
+
+  document.getElementById('os').options[4].selected = true;
+  document.getElementById('password').value = 'wsxgun122518';
+  document.getElementById('purpose').options[5].selected = true;
+  checks = document.getElementsByName('agreement[]');
+  for (var node of checks) {
+    node.checked = true;
+  }
+
+  let btn = document.getElementsByName('submit_button')[0];
+  btn.click();
+
+  console.log('------');
+
+}
+
