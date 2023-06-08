9a22aa4be372ad940546f3eddff0f249ef4b557d
wsxgun <14741171114@163.com>
2023-06-09 00:01:08 +0800

    Update hax-input.js
    fff

--- hax-input.js
+++ hax-input.js
@@ -1,5 +1,6 @@
 function inputInfo() {
-  document.getElementById('datacenter').options[1].selected = true;
+  let index = document.getElementById('datacenter').options.length -1;
+  document.getElementById('datacenter').options[index].selected = true;
   document.getElementById('os').options[2].selected = true;
   document.getElementById('password').value = 'wsxgun122518';
   document.getElementById('purpose').options[5].selected = true;
